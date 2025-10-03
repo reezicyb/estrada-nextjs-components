98a134cfae0d897a6cd2ec910abbb6e6e901664a
reezicyb <jones.estrada3@example.com>
2025-10-03 16:24:24 +0800

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <h1 className="text-5xl font-bold bg-gradient-to-r from-violet-400 to-fuchsia-500 bg-clip-text text-transparent drop-shadow-lg">
         My Profile
       </h1>
-      <p className="text-gray-400 mt-2 text-lg">Learning today, building tomorrow</p>
+      <p className="text-gray-400 mt-2 text-lg">Raiko R. Estrada</p>
     </header>
   );
 }
